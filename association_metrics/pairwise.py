12b709d9f75d42e0a65ea338aac8aa6185f8d5d9
Trujillo <heber.trj.urt@gmail.com>
2021-02-03 08:01:45 +0100

    __init__ method was created for PairWisemetrics class

--- association_metrics/pairwise.py
+++ association_metrics/pairwise.py
@@ -4,9 +4,31 @@ Created on Wed Feb  3 06:30:55 2021
 
 @author: HTRUJILLO
 """
-from pandas import DataFrame,crosstab
+from pandas import DataFrame
 
 class PairWisemetrics:
-    def __init__(self):
-        pass
+    
+    def __init__(self, dataframe, copy = True):
+        '''
+        Class initialization, it serves as a base class for all the metrics
+
+        Parameters
+        ----------
+        dataframe : pandas.DataFrame
+            Pandas dataframe containing the variables of interest to measure the degree of association.
+
+        Returns
+        -------
+        None.
+
+        '''
+        if isinstance(dataframe, DataFrame):
+        
+            if copy:
+                self.data = dataframe.copy()
+            else:
+                self.data = dataframe
+                
+        else:
+            raise TypeError("dataframe must be an instance of a pandas.DataFrame")
 
